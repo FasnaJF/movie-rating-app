d3c69793309511b1fdb16de6b387d1cc809fcb1b
FasnaJF <jffasna@gmail.com>
2025-11-16 16:37:12 +0100

    upgraded to typescipt

--- src/services/movies-service.js
+++
@@ -1,9 +0,0 @@
-import { ALL_MOVIES } from "../data/movies";
-
-export async function getMovies() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(ALL_MOVIES.items);
-    }, 1000);
-  });
-}
